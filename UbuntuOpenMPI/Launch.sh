b98f7dc349ed37e2be7ffca07b0acb25efd3acbd
Gianfranco Verrocchi <20362226+gianv9@users.noreply.github.com>
2019-02-15 15:48:35 -0400

    Testing UbuntuOpenMPI hwloc bug fix

--- UbuntuOpenMPI/Launch.sh
+++ UbuntuOpenMPI/Launch.sh
@@ -7,3 +7,5 @@ echo 'Adding node IP addresses'
 docker exec -it Test_mpi_head.1.$(docker service ps -f 'name=Test_mpi_head.1' Test_mpi_head -q --no-trunc | head -n1) sh -c "./get_hosts Test_mpi_node > /etc/openmpi/openmpi-default-hostfile"
 sleep 10s
 echo 'Cluster successfully configured'
+
+# ssh -i ssh/id_rsa.mpi -p $( source echo_head_port.sh ) mpiuser@172.18.0.1
