b1a45244bcc88e63219f4b9c8c4a49b37924a8c8
José Francisco Hernández Balestrini <kikohernandez92@me.com>
2019-02-19 20:38:36 -0400

    OpenMPI Fix thanks to Ubuntu 18.04 update, OpenMPI compiled with MultiThread support

--- UbuntuOpenMPI/Launch.sh
+++ UbuntuOpenMPI/Launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 docker stack deploy --compose-file docker-compose.yml Test
 echo 'Please wait a couple of minutes until the cluster finishes configuring itself'
 sleep 2m
@@ -7,5 +6,7 @@ echo 'Adding node IP addresses'
 docker exec -it Test_mpi_head.1.$(docker service ps -f 'name=Test_mpi_head.1' Test_mpi_head -q --no-trunc | head -n1) sh -c "./get_hosts Test_mpi_node > /etc/openmpi/openmpi-default-hostfile"
 sleep 10s
 echo 'Cluster successfully configured'
-
-# ssh -i ssh/id_rsa.mpi -p $( source echo_head_port.sh ) mpiuser@172.18.0.1
+echo 'Logging in'
+sleep 5s
+chmod 0600 ssh/id_rs*
+ssh -i ssh/id_rsa.mpi -p $( source echo_head_port.sh ) mpiuser@172.18.0.1
