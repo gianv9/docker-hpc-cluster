c4349f07043f08003fb934583095804c23359053
Gianfranco Verrocchi <20362226+gianv9@users.noreply.github.com>
2019-03-02 13:15:24 -0400

    Integrated the nfs server service

--- alpine-mpich/cluster/KillStack.sh
+++ alpine-mpich/cluster/KillStack.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-docker stack rm test
+docker stack rm alpine-mpich network_and_nfs
+
+docker-compose -f network-and-nfs.yml down
 
 # docker-compose -f launch-registry.yml down
--- alpine-mpich/cluster/LaunchStack.sh
+++ alpine-mpich/cluster/LaunchStack.sh
@@ -13,15 +13,24 @@
 # export NODE_IP=172.17.0.1
 # docker-compose -f launch-registry.yml up -d
 
+export REGISTRY_ADDR=$masterNodeIP IMAGE_NAME=gianv9/docker-hpc:alpine-mpich-latest REPLICAS=$1
+
+echo "===> Building image..."
+
 docker-compose -f upload-image-locally.yml build
+
+echo "===> Pushing image to the repository..."
 
 docker-compose -f upload-image-locally.yml push
 
-export REGISTRY_ADDR=$masterNodeIP
-export IMAGE_NAME=gianv9/docker-hpc:alpine-mpich-latest
-export REPLICAS=$1
+echo "===> Creatin the network and the nfs server container..."
 
-docker stack deploy --compose-file docker-compose.yml test
+docker-compose -f network-and-nfs.yml up -d
+# docker stack deploy --compose-file network-and-nfs.yml network_and_nfs
+
+echo "===> Deploying the cluster stack with $REPLICAS workers..."
+
+docker stack deploy --compose-file docker-compose.yml alpine-mpich
 
 echo "Waiting for The master to spawn..."
-./wait-for-it.sh 172.17.0.1:2222 -- ssh -i ssh/id_rsa -o "StrictHostKeyChecking no" -p 2222 mpi@172.17.0.1
+./wait-for-it.sh -t 60 172.17.0.1:2222 -- ssh -i ssh/id_rsa -o "StrictHostKeyChecking no" -p 2222 mpi@172.17.0.1
