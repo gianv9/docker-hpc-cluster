87d34210e996923b3bffb840c5740a794e615389
Gianfranco Verrocchi <20362226+gianv9@users.noreply.github.com>
2019-03-11 18:54:46 -0400

    Updating UbuntuOpenMPI info

--- UbuntuOpenMPI/Launch.sh
+++ UbuntuOpenMPI/Launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker stack deploy --compose-file docker-compose.yml Test
 echo 'Please wait a couple of minutes until the cluster finishes configuring itself'
-# sleep 2m
+sleep 30
 echo 'Adding node IP addresses'
 # ./wait-for-it.sh 172.17.0.1:$( source echo_head_port.sh ) -- ssh -i ssh/id_rsa.mpi -p $( source echo_head_port.sh ) root@172.17.0.1 'sh -c "./get_hosts Test_mpi_node > /etc/openmpi/openmpi-default-hostfile"'
 ./wait-for-it.sh 172.17.0.1:$( source echo_head_port.sh ) -- docker exec -it Test_mpi_head.1.$(docker service ps -f 'name=Test_mpi_head.1' Test_mpi_head -q --no-trunc | head -n1) sh -c "./get_hosts Test_mpi_node > /etc/openmpi/openmpi-default-hostfile"
